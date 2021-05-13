9dab1fe6c3bbacab590306e8cef0c4d77418ce1a
seakeene <seakeene@pdx.edu>
2021-05-13 11:18:25 -0700

    Updated README.md

--- LogTest.py
+++ LogTest.py
@@ -16,7 +16,7 @@ import json
 import pandas as pd
 import csv
 
-global simulation_id, end_program, SIMULATION_TIME, flag, w, log_timestamps, start_time, simulation_time
+global simulation_id, end_program, SIMULATION_TIME, flag, w, log_timestamps, start_time, simulation_time, flag
 start_time = 1570041113
 simulation_time = 0
 SIMULATION_TIME = 0
@@ -24,80 +24,95 @@ end_program=False
 log_timestamps = []
 flag = 0
 
-def callback(headers, message):
-    global end_program, flag, w, log_timestamps
-    log_timestamp = []
-    publish_to_topic = simulation_input_topic(simulation_id)
-    if type(message) == str:
-            message = json.loads(message)
-    if 'message' not in message:
-        if message['processStatus'] == 'COMPLETE' or \
-           message['processStatus']=='CLOSED':
-            print('Ending Program...')
-            end_program = True
-    else:
-        #Uncomment for troubleshooting
-        #print(message)
-        SIMULATION_TIME = message["message"]["timestamp"]
-        print(SIMULATION_TIME)
-        '''
-        Only runs the first output query. Grabs the keys from the message, looks them up for their real names, and
-        writes the header with the real names while still using the mrids as headers for dictwriter purposes.
-        '''
-        if flag == 0:
-            header_mrids = message['message']['measurements'].keys()
-            header_names = []
-            #print(header_mrids)
-            for i in header_mrids:
-                lookup_mrid = next(item for item in meas_object_list if item['measid'] == i)
-                lookup_name = lookup_mrid['name']
-                header_names.append(lookup_name)
-            w = csv.DictWriter(f, header_mrids)
-            #print(header_names)
-            header_names_dict = dict(zip(list(header_mrids),header_names))
-            w.writerow(header_names_dict)
-            flag = 1
-        else:
-            pass
+# def callback(headers, message):
+#     global end_program, flag, w, log_timestamps
+#     log_timestamp = []
+#     publish_to_topic = simulation_input_topic(simulation_id)
+#     if type(message) == str:
+#             message = json.loads(message)
+#     if 'message' not in message:
+#         if message['processStatus'] == 'COMPLETE' or \
+#            message['processStatus']=='CLOSED':
+#             print('Ending Program...')
+#             end_program = True
+#     else:
+#         #Uncomment for troubleshooting
+#         #print(message)
+#         SIMULATION_TIME = message["message"]["timestamp"]
+#         print(SIMULATION_TIME)
+#         '''
+#         Only runs the first output query. Grabs the keys from the message, looks them up for their real names, and
+#         writes the header with the real names while still using the mrids as headers for dictwriter purposes.
+#         '''
+#         if flag == 0:
+#             header_mrids = message.keys()
+#             header_names = []
+#             #print(header_mrids)
+#             for i in header_mrids:
+#                 lookup_mrid = next(item for item in meas_object_list if item['measid'] == i)
+#                 lookup_name = lookup_mrid['name']
+#                 header_names.append(lookup_name)
+#             w = csv.DictWriter(f, header_mrids)
+#             print(header_names)
+#             header_names_dict = dict(zip(list(header_mrids),header_names))
+#             w.writerow(header_names_dict)
+#             flag = 1
+#         else:
+#             pass
+#
+#         #print(type(message))
+#         w.writerow(message['message']['measurements'])
+#         log_timestamps.append(SIMULATION_TIME)
 
-        #print(type(message))
-        w.writerow(message['message']['measurements'])
-        log_timestamps.append(SIMULATION_TIME)
-
-def callback2(headers, message):
-    global end_program, SIMULATION_TIME, log_timestamps
-    publish_to_topic = simulation_log_topic(simulation_id)
-    if type(message) == str:
-            message = json.loads(message)
-    #print(message)
-
-    if 'message' not in message:
-        if message['processStatus'] == 'COMPLETE' or \
-           message['processStatus']=='CLOSED':
-            print('Adding timestamps to logs...')
-            f.close()
-            #Use pandas to append timestamps
-            csv_input = pd.read_csv('csvwritertest.csv')
-            #print(csv_input)
-            #print(log_timestamps)
-            log_timestamps = pd.to_datetime(log_timestamps, unit='s')
-            csv_input['Timecode'] = log_timestamps
-            movecolumn = csv_input.pop("Timecode")
-            csv_input.insert(0, "Timecode", movecolumn)
-            csv_input.to_csv('csvwritertest.csv', index=False)
-            print("Ending Simulation...")
-            end_program = True
-            quit()
-    else:
-        print('test')
+# def callback2(headers, message):
+#     global end_program, SIMULATION_TIME, log_timestamps
+#     publish_to_topic = simulation_log_topic(simulation_id)
+#     if type(message) == str:
+#             message = json.loads(message)
+#     #print(message)
+#
+#     if 'message' not in message:
+#         if message['processStatus'] == 'COMPLETE' or \
+#            message['processStatus']=='CLOSED':
+#
+#     else:
+#         print('test')
 
 
 
 def onmeasurement(sim, timestamp, measurements):
+    global end_program, flag, w, log_timestamps
     print('Measurement Test')
-    print(sim)
-    print(timestamp)
+    #print(sim)
+    #print(timestamp)
     print(measurements)
+    SIMULATION_TIME = timestamp
+    print(SIMULATION_TIME)
+    '''
+    Only runs the first output query. Grabs the keys from the message, looks them up for their real names, and
+    writes the header with the real names while still using the mrids as headers for dictwriter purposes.
+    '''
+    if flag == 0:
+        header_mrids = measurements.keys()
+        header_names = []
+        print(header_mrids)
+        for i in header_mrids:
+            lookup_mrid = next(item for item in meas_object_list if item['measid'] == i)
+            lookup_name = lookup_mrid['name']
+            header_names.append(lookup_name)
+        w = csv.DictWriter(f, header_mrids)
+        print(header_names)
+        header_names_dict = dict(zip(list(header_mrids), header_names))
+        w.writerow(header_names_dict)
+        flag = 1
+    else:
+        pass
+
+    # print(type(message))
+    w.writerow(measurements)
+    log_timestamps.append(SIMULATION_TIME)
+
+
 
 def ontimestep(sim, timestep):
     '''
@@ -112,41 +127,78 @@ def ontimestep(sim, timestep):
     print("After:")
     simulation_time += 1
     print(simulation_time)
+    try:
+        input_at_time_now = next(item for item in table_from_csvin if int(item['Time']) >= simulation_time and int(item['Time']) < simulation_time+1)
+        operate_switch(int(input_at_time_now['Switch']))
+    except StopIteration:
+        print("End of input data.")
+        return
 
-    switch_time = 1570041119
-    open_switch(simulation_time, switch_time)
 
 
 def onstart(sim):
-    global start_time, simulation_time, input_topic
+    global start_time, simulation_time, input_topic, table_from_csvin
     print("Start time:")
     print(start_time)
     simulation_time = start_time
+    table_from_csvin = process_input_csv('input.csv')
+
+def oncomplete(sim):
+    global end_program, log_timestamps
+    print("Simulation complete.")
+    print('Adding timestamps to logs...')
+    f.close()
+    # Use pandas to append timestamps
+    csv_input = pd.read_csv('csvwritertest.csv')
+    # print(csv_input)
+    # print(log_timestamps)
+    log_timestamps = pd.to_datetime(log_timestamps, unit='s')
+    csv_input['Timecode'] = log_timestamps
+    movecolumn = csv_input.pop("Timecode")
+    csv_input.insert(0, "Timecode", movecolumn)
+    csv_input.to_csv('csvwritertest.csv', index=False)
+    print("Ending Simulation...")
+    end_program = True
+    quit()
 
 
-def open_switch(time, open_time):
-    '''
-    Function to open the switch at a given time.
-    time: the current simulation time as kept by ontimestep()
-    open_time: the timecode at which the switch should be opened (I.E. taken from an input
-                .csv of timecoded switch position values)
-    '''
-    # Check if it is time for a switch operation.
-    if time == open_time:
-        print("Opening switch, should happen once")
-        #Create the DifferenceBuilder to automate message construction.
-        db = DifferenceBuilder(simulation_id)
+def operate_switch(position):
+        global input_topic, db
+        if position == 1:
+            print('Closing Switch:')
+            # Create the DifferenceBuilder to automate message construction.
+            db1 = DifferenceBuilder(simulation_id)
+            # Generate the proper message to open the switch.
+            db1.add_difference(sw_mrid, "Switch.open", 0, 1)
+            switch_message1 = db1.get_message()
+            print(switch_message1)
+            # Send message to the input topic (opened in onstart())
+            switch_message = json.dumps(switch_message1)
+            gapps.send(input_topic, switch_message)
 
-        #Generate the proper message to open the switch.
-        #TROUBLESHOOT: Switch not opening, i think
-        db.add_difference(sw_mrid, "Switch.open", 1, 0)
-        switch_message = db.get_message()
-        print(switch_message)
+        elif position == 0:
+            print('Opening Switch:')
+            # Create the DifferenceBuilder to automate message construction.
+            db2 = DifferenceBuilder(simulation_id)
+            # Generate the proper message to open the switch.
+            db2.add_difference(sw_mrid, "Switch.open", 1, 0)
+            switch_message2 = db2.get_message()
+            print(switch_message2)
+            # Send message to the input topic (opened in onstart())
+            switch_message2 = json.dumps(switch_message2)
+            gapps.send(input_topic, switch_message2)
+        else:
+            print('error')
 
-        #Send message to the input topic (opened in onstart())
-        switch_message = json.dumps(switch_message)
-        gapps.send(input_topic, switch_message)
 
+
+def process_input_csv(inputfile):
+    with open(inputfile) as csvfile:
+        r = csv.DictReader(csvfile)
+        x = []
+        for row in r:
+            x.append(row)
+    return x
 
 #Connect to GridAPPS
 gapps = GridAPPSD("('localhost', 61613)", username='system', password='manager')
@@ -259,18 +311,19 @@ simulation_id = simulation.simulation_id
 simulation.add_onstart_callback(onstart)
 simulation.add_ontimestep_callback(ontimestep)
 simulation.add_onmesurement_callback(onmeasurement)
+simulation.add_oncomplete_callback(oncomplete)
 simulation.start_simulation()
 input_topic = simulation_input_topic(simulation_id)
 
 print(simulation_id)
 
 #Test the callback function
-sim_output_topic = simulation_output_topic(simulation_id)
+# sim_output_topic = simulation_output_topic(simulation_id)
 f = open('csvwritertest.csv', 'w')
 
-gapps.subscribe(sim_output_topic, callback)
-sim_log_topic = simulation_log_topic(simulation_id)
-gapps.subscribe(sim_log_topic, callback2)
+# gapps.subscribe(sim_output_topic, callback)
+# sim_log_topic = simulation_log_topic(simulation_id)
+#gapps.subscribe(sim_log_topic, callback2)
 
 
 
@@ -281,8 +334,6 @@ def _main():
     print('test')
     while not end_program:
         time.sleep(0.1)
-        if SIMULATION_TIME >= 1570041121:
-            print("Test!")
 
     if end_program:
         f.close()
