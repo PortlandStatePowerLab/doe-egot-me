c113492663dc43ce8f3f882b7f2fc7fa9cc2aaad
seanjkeene <seakeene@pdx.edu>
2022-11-21 12:19:42 -0800

    Tests cleaned up. 28/23/37 steps complete.

--- features/environment.py
+++ features/environment.py
@@ -2,7 +2,17 @@ from behave import *
 import ModelController
 
 def before_all(context):
+    context.MEPath = "/home/seanjkeene/PycharmProjects/doe-egot-me/"
+
+    print("First run...")
     try:
-        ModelController.main(test_mode=True)
+        ModelController.main(test_mode=False, DERSHDI_FilePath=r"DERSHistoricalData Inputs/TP_ME1_A_LogInput.csv")
     except SystemExit:
-        pass
+        context.firstfilename = ModelController.mcConfiguration.output_log_name
+        context.firstfilepath = context.MEPath + context.firstfilename
+    print("Second run...")
+    try:
+        ModelController.main(test_mode=False, DERSHDI_FilePath=r"DERSHistoricalData Inputs/TP_ME1_A_LogInput2.csv")
+    except SystemExit:
+        context.secondfilename = ModelController.mcConfiguration.output_log_name
+        context.secondfilepath = context.MEPath + context.secondfilename
