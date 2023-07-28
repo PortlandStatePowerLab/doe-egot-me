fb2e3293ac5b45ba17711c05f19c187197e8e64e
MidrarAdham <midrar@pdx.edu>
2023-07-28 12:49:57 -0700

    prepping for merge. Removed all measurement file in DERScript/Meas/ folder so I don't overwrite testing files. These are generated by GridAPPS-D but I'm being cautious

---
+++ DERScripts/drop_der.sh
@@ -0,0 +1,5 @@
+source envars.sh
+
+#python3 $CIMHUB_UTILS/DropDER.py ../cimhubconfig.json acep_psil_der_uuid.txt
+#python3 $CIMHUB_UTILS/DropDER.py ../cimhubconfig.json test9500new_der_uuid.txt
+python3 $CIMHUB_UTILS/DropDER.py $CIMHUB_UTILS/cimhubconfig.json EGoT13_der_uuid.txt
